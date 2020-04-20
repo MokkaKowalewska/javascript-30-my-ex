2060b7c20e18aa6507d3d18a740db448df92d51d
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-20 02:10:23 +0200

    init: query selectors into variables

---
+++ javascript30-11-CustomVideoPlayer/scripts.js
@@ -0,0 +1,10 @@
+const _ = (selector) => {
+    return document.querySelector(selector);
+  },
+  player = _(".player"),
+  play = _(".player__button"),
+  ranges = _(".player__slider"),
+  viewer = _(".viewer"),
+  progress = _(".progress"),
+  progressBar = _(".progress__filled"),
+   = _("."),
